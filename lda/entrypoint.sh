4c622446a739be6963257a37e2866466ef9feaf9
Yuanhao Ji <tsund@qq.com>
2022-01-30 13:10:28 +0800

    fix(kmeans, lda): fix entrypoint.sh and parameter in lda/pom.xml

--- lda/entrypoint.sh
+++ lda/entrypoint.sh
@@ -6,19 +6,19 @@ none='\e[0m'
 file=$1
 cmd="java $JAVA_OPTS -jar $file"
 
-if [[ $MASTER_URL ]]; then
+if [ $MASTER_URL ]; then
   cmd="$cmd --master $MASTER_URL"
 fi
 
-if [[ $TRAIN_DATASET_PATH ]]; then
+if [ $TRAIN_DATASET_PATH ]; then
   cmd="$cmd --train-dataset $TRAIN_DATASET_PATH"
 fi
 
-if [[ $TEST_DATASET_PATH ]]; then
+if [ $TEST_DATASET_PATH ]; then
   cmd="$cmd --test-dataset $TEST_DATASET_PATH"
 fi
 
-if [[ $VALIDATE_DATASET_PATH ]]; then
+if [ $VALIDATE_DATASET_PATH ]; then
   cmd="$cmd --validate-dataset $VALIDATE_DATASET_PATH"
 fi
 
